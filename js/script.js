79adfadd8988833fd13cef0762f97bc411f0d075
Yasz Avellia <zakamaragames@gmail.com>
2022-09-20 21:07:48 +0700

    ngetes ajax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('ok')
+let keyword = document.getElementById("keyword");
+let tombolCari = document.getElementById("tombol-cari");
+let container = document.getElementById("container");
+
+keyword.addEventListener("keyup", function () {
+  //object ajax
+  let xhr = new XMLHttpRequest();
+
+  // cek kesiapan ajax
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log("ajax ok");
+    }
+  };
+
+  xhr.open("GET", "ajax/coba.txt", true);
+  xhr.send();
+});
